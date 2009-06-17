2924fef192b2fa34ea01311c17f235819975b4d9
Louis Lagrange <lagrange.louis@gmail.com>
2009-06-17 19:39:40 +0000

    oops, forget to add these files

---
+++ include/ML_backgrounds.h
@@ -0,0 +1,20 @@
+#ifndef DEF_ML_BACKGROUND_H
+#define DEF_ML_BACKGROUND_H
+
+/**
+* \file
+* \brief This file contains backgrounds headers.
+*/
+
+#ifdef __cplusplus
+   extern "C" {
+#endif /* __cplusplus */
+
+inline void _initBackground(ML_Background *background);
+
+#ifdef __cplusplus
+   }
+#endif /* __cplusplus */
+
+#endif
+
---
+++ include/ML_images.h
@@ -0,0 +1,35 @@
+#ifndef DEF_ML_IMAGES_H
+#define DEF_ML_IMAGES_H
+
+/**
+* \file
+* \brief This file contains images headers.
+*/
+
+#ifdef __cplusplus
+   extern "C" {
+#endif /* __cplusplus */
+
+inline void _initImage(ML_Image *image);
+
+/**
+* \fn void ML_CloneImage(ML_Image *image1, ML_Image *image2)
+* \brief This function clones two images.
+* @param image1 The original image
+* @param image2 The image which will be the same as image1
+*/
+extern void ML_CloneImage(ML_Image *image1, ML_Image *image2);
+
+/**
+* \fn void ML_DeleteImage(ML_Image *image)
+* \brief This function deletes an image. Call it when exiting the app/game or if you don't use it anymore.
+* @param image The image which will be deleted
+*/
+extern void ML_DeleteImage(ML_Image *image);
+
+#ifdef __cplusplus
+   }
+#endif /* __cplusplus */
+
+#endif
+
