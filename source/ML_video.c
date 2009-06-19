9a81237f149ca227c67756fbae29af967dcd9578
lagrange.louis@gmail.com <lagrange.louis@gmail.com@a40315b2-fe47-0410-81ff-5bdf4d21b671>
2009-06-19 21:27:09 +0000

    (oops, forgot to add these files)

--- source/ML_video.c
+++ source/ML_video.c
@@ -165,42 +165,6 @@ void ML_SetBackgroundColor(GXColor color)
 	GX_SetCopyClear(color, 0x00ffffff);	
 }
 
-// These two next functions are mainly inspired from GRRLIB, thank you for these equation/algorithm !
-void ML_SetPixelColor(ML_Image *image, int x, int y, u32 color) 
-{
-	u8 *truc = (u8*)image->data;
-	u32 offset;
-
-	offset = (((y >> 2)<<4)*image->width) + ((x >> 2)<<6) + (((y%4 << 2) + x%4 ) <<1); // Fuckin equation found by NoNameNo ;)
-
-	*(truc+offset)=(color) & 0xFF;
-	*(truc+offset+1)=(color>>24) & 0xFF;
-	*(truc+offset+32)=(color>>16) & 0xFF;
-	*(truc+offset+33)=(color>>8) & 0xFF;
-	
-	image->data = truc;
-	
-	DCFlushRange(image->data, image->width * image->height * 4);
-	GX_InitTexObj(&image->texObj, image->data, image->width, image->height, GX_TF_RGBA8, GX_CLAMP, GX_CLAMP, GX_FALSE);	
-	GX_InitTexObjLOD(&image->texObj, GX_NEAR, GX_NEAR, 0.0f, 0.0f, 0.0f, 0, 0, GX_ANISO_1);
-}
-
-u32 ML_GetPixelColor(ML_Image *image, int x, int y) 
-{
-	u8 *truc = (u8*)image->data;
-	u8 r, g, b, a;
-	u32 offset;
-
-	offset = (((y >> 2)<<4)*image->width) + ((x >> 2)<<6) + (((y%4 << 2) + x%4 ) << 1); // Fuckin equation found by NoNameNo ;)
-
-	a=*(truc+offset);
-	r=*(truc+offset+1);
-	g=*(truc+offset+32);
-	b=*(truc+offset+33);
-
-	return((r<<24) | (g<<16) | (b<<8) | a);
-}
-
 void ML_SplashScreen()
 {
 	int i = 0;
@@ -209,8 +173,6 @@ void ML_SplashScreen()
 	ML_Image image;
 	_initImage(&image);
 	ok = read_png_gx_file_buffer(MLlib_SplashScreen_png, &image);
-	
-	//_alphaFade = 255;
 	
 	if(!ok)
 		return;
