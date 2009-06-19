ea452556693a70f2d00a71114e9590dc7e7f55e3
lagrange.louis@gmail.com <lagrange.louis@gmail.com@a40315b2-fe47-0410-81ff-5bdf4d21b671>
2009-06-19 21:26:34 +0000

    - added: ML_FlipImageX(ML_Image *image); - added: ML_FlipImageY(ML_Image *image); - added: ML_InvertImageColors(ML_Image *image);

--- source/ML_images.c
+++ source/ML_images.c
@@ -26,3 +26,75 @@ void ML_DeleteImage(ML_Image *image)
 	}
 }
 
+// These two next functions are mainly inspired from GRRLIB, thank you for these equation/algorithm !
+void ML_SetPixelColor(ML_Image *image, int x, int y, u32 color) 
+{
+	u8 *truc = (u8*)image->data;
+	u32 offset;
+
+	offset = (((y >> 2)<<4)*image->width) + ((x >> 2)<<6) + (((y%4 << 2) + x%4 ) <<1); // Fuckin equation found by NoNameNo ;)
+
+	*(truc+offset)=(color) & 0xFF;
+	*(truc+offset+1)=(color>>24) & 0xFF;
+	*(truc+offset+32)=(color>>16) & 0xFF;
+	*(truc+offset+33)=(color>>8) & 0xFF;
+	
+	image->data = truc;
+	
+	DCFlushRange(image->data, image->width * image->height * 4);
+	GX_InitTexObj(&image->texObj, image->data, image->width, image->height, GX_TF_RGBA8, GX_CLAMP, GX_CLAMP, GX_FALSE);	
+	GX_InitTexObjLOD(&image->texObj, GX_NEAR, GX_NEAR, 0.0f, 0.0f, 0.0f, 0, 0, GX_ANISO_1);
+}
+
+u32 ML_GetPixelColor(ML_Image *image, int x, int y) 
+{
+	u8 *truc = (u8*)image->data;
+	u8 r, g, b, a;
+	u32 offset;
+
+	offset = (((y >> 2)<<4)*image->width) + ((x >> 2)<<6) + (((y%4 << 2) + x%4 ) << 1); // Fuckin equation found by NoNameNo ;)
+
+	a=*(truc+offset);
+	r=*(truc+offset+1);
+	g=*(truc+offset+32);
+	b=*(truc+offset+33);
+
+	return((r<<24) | (g<<16) | (b<<8) | a);
+}
+
+void ML_FlipImageX(ML_Image *image)
+{
+	unsigned int x, y, texWidth = image->width - 1;
+
+	for (y = 0; y < image->height; y++) {
+		for (x = 0; x < image->width; x++) {
+			ML_SetPixelColor(image, texWidth - x, y, ML_GetPixelColor(image, x, y));
+		}
+	}
+}
+
+void ML_FlipImageY(ML_Image *image) 
+{
+	unsigned int x, y, texHeight = image->height - 1;
+
+	for (y = 0; y < image->height; y++) {
+		for (x = 0; x < image->width; x++) {
+			ML_SetPixelColor(image, x, texHeight - y, ML_GetPixelColor(image, x, y));
+		}
+	}
+}
+
+void ML_InvertImageColors(ML_Image *image)
+{
+	unsigned int x, y;
+	u32 color;
+
+	for (y = 0; y < image->height; y++) {
+		for (x = 0; x < image->width; x++) {
+			color = ML_GetPixelColor(image, x, y);
+			ML_SetPixelColor(image, x, y, ((0xFFFFFF - (color >> 8 & 0xFFFFFF)) << 8)  | (color & 0xFF));
+		}
+	}
+}
+
+
