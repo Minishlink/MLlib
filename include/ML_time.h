5c38228059c335789373d0edac8900d0abc14f7f
Louis Lagrange <lagrange.louis@gmail.com>
2009-08-31 17:23:19 +0000

    - added: ML_InitRand() - added: ML_Rand(min, max)

--- include/ML_time.h
+++ include/ML_time.h
@@ -3,7 +3,7 @@
 
 /**
 * \file
-* \brief This file contains time headers.
+* \brief This file contains time and random headers.
 */
 
 #ifdef __cplusplus
@@ -11,18 +11,33 @@
 #endif /* __cplusplus */
 
 /**
-* \fn extern time_t ML_GetTime()
+* \fn time_t ML_GetTime()
 * \brief This function gets RTC time.
 * @return Return time in a time_t struct.
 */
 extern time_t ML_GetTime();
 
 /**
-* \fn extern char* ML_GetTimeChar()
+* \fn char* ML_GetTimeChar()
 * \brief This function gets RTC time in char.
 * @return Return time in char.
 */
 extern char* ML_GetTimeChar();
+
+/**
+* \fn int ML_InitRand()
+* \brief This function initializes the random system. Call this once.
+*/
+extern void ML_InitRand();
+
+/**
+* \fn int ML_Rand(int min, int max)
+* \brief This function returns a random number between "min" and "max". Call this function after ML_InitRand().
+* @param min Minimum number
+* @param max Maximum number
+* @return Return the random number.
+*/
+extern int ML_Rand(int min, int max);
 
 #ifdef __cplusplus
    }
