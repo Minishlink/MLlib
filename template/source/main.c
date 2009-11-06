d38ccbf82c6fe9afa1dd4381addaad1d309f7c96
lagrange.louis@gmail.com <lagrange.louis@gmail.com@a40315b2-fe47-0410-81ff-5bdf4d21b671>
2009-11-06 18:53:30 +0000

    It's safe to update libfat to the lastest version. It's NOT safe for DevkitPPC r19 on Linux (I'm not sure on Windows and OSX) because the compiler is broken, we just have to wait for a future release of devkitPPC... So : update libfat but not devkitPPC. - added: template in the SVN. You must have the lastest Makefile of this template if you want to compile your apps. You also need FreeType library installed since r51.

---
+++ template/source/main.c
@@ -0,0 +1,16 @@
+#include <MLlib.h>
+
+int main(int argc, char **argv) 
+{	ML_Init();
+	ML_SplashScreen(); // You can delete this instruction, but please let it if you can :)
+		
+	while(1)
+	{
+		if(Wiimote[0].Newpress.Home) ML_Exit();
+		
+		ML_Refresh();
+	}
+	
+	return 0;
+}
+
